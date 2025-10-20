de42faeb9b8d146f97249e080346ba766e21aa24
Fabio <foli.brondix@gmail.com>
2025-10-20 10:15:27 +0200

    Initial commit

---
+++ src/main/java/com/generation/progettospesupbackend/ProgettoSpesUpBackEndApplication.java
@@ -0,0 +1,15 @@
+package com.generation.progettospesupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSpesUpBackEndApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(ProgettoSpesUpBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/progettospesupbackend/ProgettoSpesUpBackEndApplicationTests.java
@@ -0,0 +1,15 @@
+package com.generation.progettospesupbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSpesUpBackEndApplicationTests
+{
+
+	@Test
+	void contextLoads()
+	{
+	}
+
+}
