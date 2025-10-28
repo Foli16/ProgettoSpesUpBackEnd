543b460ab8d1b4474d06e529de4bd581dcdf1fce
Fabio <foli.brondix@gmail.com>
2025-10-28 17:40:59 +0100

    Sistemato quel maledetto Security

--- src/main/java/com/generation/progettospesupbackend/security/SecurityConfig.java
+++ src/main/java/com/generation/progettospesupbackend/security/SecurityConfig.java
@@ -15,29 +15,21 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig
 {
-    @Autowired
-    UserFilter filtro;
 
-//    @Bean
-//    public SecurityFilterChain sicurezza(HttpSecurity http) throws Exception
-//    {
-//        http
-//        .csrf(csrf -> csrf.disable())
-//        .authorizeHttpRequests(auth->
-//
-//                auth
-//                .requestMatchers("/api/auth/login", "/api/auth/register").permitAll()//tutti, no controlli
-//                .requestMatchers("/api/auth/userinformation").hasRole("STANDARD")
-//                .requestMatchers("/api/adminController/**").hasRole("ADMIN")//tutti i metodi che hanno come prefisso /api/adminController/** li puoi richiamare solo se sei loggato come admin
-//                .requestMatchers(HttpMethod.POST).authenticated()
-//                .requestMatchers(HttpMethod.PUT).authenticated()
-//                .requestMatchers(HttpMethod.DELETE).authenticated()
-//                .anyRequest().permitAll()//devi essere loggato
-//
-//        ).addFilterBefore(filtro, UsernamePasswordAuthenticationFilter.class);//dove viene fatto, sempre uguale
-//
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain sicurezza(HttpSecurity http, UserFilter filtro) throws Exception
+    {
+        http
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(auth->
+
+                auth
+                .anyRequest().permitAll()//devi essere loggato
+
+        ).addFilterBefore(filtro, UsernamePasswordAuthenticationFilter.class);//dove viene fatto, sempre uguale
+
+        return http.build();
+    }
 
     //creo bean del criptatore da autowirare
     @Bean
