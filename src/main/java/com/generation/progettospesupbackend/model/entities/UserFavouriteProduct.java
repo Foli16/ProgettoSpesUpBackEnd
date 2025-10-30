2fc1dc72835cffeef1c4b7b9b02409f6ce8c11a6
Fabio <foli.brondix@gmail.com>
2025-10-30 12:30:55 +0100

    Sistemata mappa per visualizzare relazioni di User con Product nel db

---
+++ src/main/java/com/generation/progettospesupbackend/model/entities/UserFavouriteProduct.java
@@ -0,0 +1,26 @@
+package com.generation.progettospesupbackend.model.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class UserFavouriteProduct extends BaseEntity {
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "product_id")
+	private Product product;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "supermarket_id")
+	private Supermarket supermarket;
+}
